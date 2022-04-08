c629d4525e1b0da1e6ec00b79175e1f5238e2c9c
Ray <rachel!7.22@gmail.com>
2022-04-08 13:56:03 -0700

    prettier setup on commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const port = 4000;
 
-app.get('/api/addresses', (_, res) => {
-    res.json({
-        addresses: [
-            {
-                name: "Ned Stark",
-                address1: "123 Main Street",
-                address2: "apt# 1",
-                city: "Winterfell",
-                state: "cold",
-                zip: 12345,
-                phone: "925-2555-351"
-            }
-        ]
-
-    })
-})
+app.get("/api/addresses", (_, res) => {
+  res.json({
+    addresses: [
+      {
+        name: "Ned Stark",
+        address1: "123 Main Street",
+        address2: "apt# 1",
+        city: "Winterfell",
+        state: "cold",
+        zip: 12345,
+        phone: "925-2555-351",
+      },
+    ],
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Address Book server running at http://localhost:${port}`)
-})
+  console.log(`Address Book server running at http://localhost:${port}`);
+});
