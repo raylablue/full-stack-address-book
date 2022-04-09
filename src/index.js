10e32ef5e8110310611f3a9c9e2a3091feddb90d
Ray <rachel!7.22@gmail.com>
2022-04-08 18:10:52 -0700

    initial post setup for server

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const { add } = require("husky");
+
 const port = 4000;
+const addresses = [];
+
+const app = express();
+app.use(bodyParser.json());
 
 app.get("/api/addresses", (_, res) => {
   res.json({
-    addresses: [
-      {
-        name: "Ned Stark",
-        address1: "123 Main Street",
-        address2: "apt# 1",
-        city: "Winterfell",
-        state: "cold",
-        zip: 12345,
-        phone: "925-2555-351",
-      },
-    ],
+    addresses,
   });
 });
 
+app.post("/api/addresses", (req, res) => {
+  addresses.push(req.body);
+  res.send(req.body);
+});
+
 app.listen(port, () => {
-  console.log(`Address Book server running at http://localhost:${port}`);
+  console.log(
+    `Address Book server running at http://localhost:${port}/api/addresses`
+  );
 });
